d32fe582ad6c606785887301988d7aad12f58fac
Creampanda <andrewfromtokyo@gmail.com>
2019-10-18 17:35:44 +0300

    Реализовал умножение числа на число

--- BigNumbers/BigNumber.h
+++ BigNumbers/BigNumber.h
@@ -13,10 +13,12 @@ public:
 	{
 		
 		if (this->vectorNumber_.size() < other.vectorNumber_.size())
-		{
 			this->vectorNumber_.swap(other.vectorNumber_);
-		}
+
+
 		vector<int> vectorResult(this->vectorNumber_.size() + 1, 0);
+
+
 		for (size_t i = 0; i < this->vectorNumber_.size(); i++)
 		{
 
@@ -41,6 +43,37 @@ public:
 		return temp;
 		
 	};
+
+	BigNumber operator*(BigNumber other)
+	{
+
+		if (this->vectorNumber_.size() < other.vectorNumber_.size())
+			this->vectorNumber_.swap(other.vectorNumber_);
+
+		vector<int> vectorResult(this->vectorNumber_.size() + other.vectorNumber_.size() +1, 0);
+
+
+		for (size_t i = 0; i < other.vectorNumber_.size(); i++)
+		{
+			for (size_t j = 0; j < this->vectorNumber_.size(); j++)
+				vectorResult[i+j] = vectorResult[i+j] + other.vectorNumber_[i] * this->vectorNumber_[j];
+		}
+
+		for (size_t i = 0; i < vectorResult.size() - 1; i++)
+		{
+			vectorResult[i + 1] += vectorResult[i] / 10;
+			vectorResult[i] %= 10;
+		}
+
+		while (vectorResult.back() == 0)
+			vectorResult.pop_back();
+
+		BigNumber temp(vectorResult);
+
+		return temp;
+
+	};
+
 
 private:
 	std::vector <int> vectorNumber_;
--- BigNumbers/Source.cpp
+++ BigNumbers/Source.cpp
@@ -10,14 +10,18 @@ using namespace std;
 int main()
 {
 
-	BigNumber num1("15512532895192679275923");
+	BigNumber num1("77214215125154316137");
 	num1.showVector();
 
-	BigNumber num2("9948249852992958");
+	BigNumber num2("2125124523156124643162");
 	num2.showVector();
 
 	BigNumber num3 = num1 + num2;
 	num3.showVector();
+
+	BigNumber num4 = num1 * num2;
+	num4.showVector();
+
 	return 0;
 }
 
